429e8e72f8f401a914a89b205c88e77dbf2a7f80
kauã <kauaprestestoscano@gmail.com>
2024-10-06 17:47:29 -0300

    Update apps.py

--- tweaker/apps.py
+++ tweaker/apps.py
@@ -2,37 +2,37 @@ import subprocess
 import os
 
 def execute_autoruns():
-    caminho_bat = r"C:\kekaapp\.bat\autoruns.bat"
+    caminho_bat = os.path.join("C:", os.sep, "kekaapp", ".bat", "autoruns.bat")
     subprocess.run(caminho_bat, check=True)
 
 def execute_msi():
-    caminho_bat = r"C:\kekaapp\.bat\msiutil.bat"
+    caminho_bat = os.path.join("C:", os.sep, "kekaapp", ".bat", "msiutil.bat")
     subprocess.run(caminho_bat, check=True)
 
 def execute_pwrset():
-    caminho_bat = r"C:\kekaapp\.bat\powersettings.bat"
+    caminho_bat = os.path.join("C:", os.sep, "kekaapp", ".bat", "powersettings.bat")
     subprocess.run(caminho_bat, check=True)
 
 def execute_temp():
-    caminho_bat = r"C:\kekaapp\.bat\tempcleaner.bat"
+    caminho_bat = os.path.join("C:", os.sep, "kekaapp", ".bat", "tempcleaner.bat")
     subprocess.run(caminho_bat, check=True)
 
 def execute_unpark():
-    caminho_bat = r"C:\kekaapp\.bat\unparkcpu.bat"
+    caminho_bat = os.path.join("C:", os.sep, "kekaapp", ".bat", "unparkcpu.bat")
     subprocess.run(caminho_bat, check=True)
 
 def execute_uwt4():
-    caminho_bat = r"C:\kekaapp\.bat\uwt4.bat"
+    caminho_bat = os.path.join("C:", os.sep, "kekaapp", ".bat", "uwt4.bat")
     subprocess.run(caminho_bat, check=True)
 
 def execute_deviceCleanup():
-    caminho_bat = r"C:\kekaapp\.bat\cleanup.bat"
+    caminho_bat = os.path.join("C:", os.sep, "kekaapp", ".bat", "cleanup.bat")
     subprocess.run(caminho_bat, check=True)
 
 def execute_wub():
-    caminho_bat = r"C:\kekaapp\.bat\wub.bat"
+    caminho_bat = os.path.join("C:", os.sep, "kekaapp", ".bat", "wub.bat")
     subprocess.run(caminho_bat, check=True)
 
 def execute_dism():
-    caminho_bat = r"C:\kekaapp\.bat\dism.bat"
+    caminho_bat = os.path.join("C:", os.sep, "kekaapp", ".bat", "dism.bat")
     subprocess.run(caminho_bat, check=True)
